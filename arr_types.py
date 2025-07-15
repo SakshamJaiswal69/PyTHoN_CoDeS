8393efb7e15f2fb376a7d59ca9cbda3dfe73d778
SakshamJaiswal69 <a6136593@gmail.com>
2025-07-15 11:22:54 +0530

    Create arr_types.py

---
+++ arr_types.py
@@ -0,0 +1,10 @@
+import numpy as np
+# """these are zeros array"""
+# arr=np.zeros((4,6))
+# print(arr)
+# """these are ones array"""
+# arr1=np.ones((4,6))
+# print(arr1)
+"""now these are ggarbage value array"""
+arr2=np.empty((4,6))
+print(arr2)
