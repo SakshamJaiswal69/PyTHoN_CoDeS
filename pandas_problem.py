23493f15dc7ffe89a3083482f01d6f6cc9ee8fd4
SakshamJaiswal69 <a6136593@gmail.com>
2025-08-04 12:58:46 +0530

    Create pandas_problem.py

---
+++ pandas_problem.py
@@ -0,0 +1,17 @@
+# import pandas as pd
+# # in pandas there are 2 type of structure =series OR data frame 
+# # Series data sturcture is one dimemensieon structure (One Coloumn)
+# # a=[1,5.3,2,3,4]
+# # a=[1,5,2,3,4]
+# # a=['fj','sfj','fjds']
+# # arr=pd.Series(a)
+# # print(type(arr))
+# # print(arr)
+# data={"hallo":[21,33,45],
+#       "bye_bye":[43,46,67]
+#       }
+# s=pd.DataFrame(data)
+# print(s)
+
+
+
