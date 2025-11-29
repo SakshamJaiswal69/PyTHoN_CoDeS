34e8be719c5cb85134821b005f1a83c743935f2a
Saksham_Jaiswal69 <a6136593@gmail.com>
2025-11-29 13:05:50 +0530

    changes

--- append and insert methods.py
+++ append and insert methods.py
@@ -1,10 +1,11 @@
-a=[]
-b=int(input("enter"))
+a=[1,2,3]
+b=int(input("enter :"))
 a.append(b)
 print(a)
-c=[]
-d=int(input("position"))
-e=int(input("value"))
+c=[8,6,5,6]
+print(c)
+d=int(input("position :"))
+e=int(input("value :"))
 c.insert(d-1,e)
 print(c)
 
--- dict data count.py
+++ dict data count.py
@@ -1,6 +1,6 @@
 d1={2:'surya',5:'boom',6:'ayush',4:'ayush'}
 for a,b in d1.items:
     v=[]
+    a=()
     v.append(b)
-    print(v
-          )
+    print(v)
