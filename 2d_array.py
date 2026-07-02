cb32a82f62f8af83c216f41c7cbd2d68337a3877
Saksham_Jaiswal69 <a6136593@gmail.com>
2026-07-02 23:06:07 +0530

    Update 2d_array.py

--- 2d_array.py
+++ 2d_array.py
@@ -1,7 +1,7 @@
 import numpy as np
 # making 1  and 2 dimension array 
 at=np.array([[[12,32],[32,56],[45,88],[43,55],[63,44]]])
-# print(at.dtype)
-# print(at.ndim)
-# print(at.shape[2])
-# print(at)
+print(at.dtype)
+print(at.ndim)
+print(at.shape[2])
+print(at)
